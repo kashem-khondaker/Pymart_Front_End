0923f33c2f9ee12729fed46c7725624fd0b8ebe9
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-07-03 12:27:24 +0600

    images preview functionality added successfully

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -11,6 +11,8 @@ const AddProduct = () => {
   } = useForm();
 
   const [categories, setCategories] = useState([]);
+  const [productId, setProductId] = useState(null);
+  const [previewImages, setPreviewImages] = useState([]);
 
   useEffect(() => {
     apiClient.get("/categories/").then((response) => {
@@ -19,9 +21,9 @@ const AddProduct = () => {
     });
   }, []);
 
-  useEffect( () => {
-    console.log(errors)
-  } ,[errors])
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
   const handleProductAdd = async (data) => {
     // convert string inputs to numbers
@@ -41,6 +43,7 @@ const AddProduct = () => {
         formattedData
       );
       console.log("Product added successfully", productResponse.data);
+      setProductId(productResponse.data.id);
     } catch (error) {
       console.error(
         "Error adding product:",
@@ -49,85 +52,129 @@ const AddProduct = () => {
     }
   };
 
+  //handleImageChange
+  const handleImageChange = (event) => {
+    const files = Array.from(event.target.files);
+    // console.log(files);
+    setPreviewImages(files.map((file) => URL.createObjectURL(file)));
+  };
+
   return (
     <div className="max-w-2xl mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg ">
       <h2 className="text-2xl font-semibold mb-4">Add New Product </h2>
-      <form onSubmit={handleSubmit(handleProductAdd)} className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium">Product name</label>
-          <input
-            {...register("name", { required: true })}
-            className="input input-bordered w-full"
-            placeholder="Product Name"
-          />
-          {errors.name && (
-            <p className="text-red-500 text-xs">This field is required </p>
-          )}
-        </div>
-        <div>
-          <label className="block text-sm font-medium"> Description</label>
-          <textarea
-            {...register("description", { required: true })}
-            className="textarea textarea-bordered w-full"
-            placeholder="Description"
-          ></textarea>
-          {errors.description && (
-            <p className="text-red-500 text-xs"> This field is required </p>
-          )}
-        </div>
+      {productId ? (
+        <form onSubmit={handleSubmit(handleProductAdd)} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium">Product name</label>
+            <input
+              {...register("name", { required: true })}
+              className="input input-bordered w-full"
+              placeholder="Product Name"
+            />
+            {errors.name && (
+              <p className="text-red-500 text-xs">This field is required </p>
+            )}
+          </div>
+          <div>
+            <label className="block text-sm font-medium"> Description</label>
+            <textarea
+              {...register("description", { required: true })}
+              className="textarea textarea-bordered w-full"
+              placeholder="Description"
+            ></textarea>
+            {errors.description && (
+              <p className="text-red-500 text-xs"> This field is required </p>
+            )}
+          </div>
 
-        <div>
-          <label className="block text-sm font-medium"> Price</label>
-          <input
-            type="text"
-            {...register("price", { required: "This field is required " , validate: (value) => {
-                const inputValue = parseFloat(value);
-                return !isNaN(inputValue) || "Please input a valid number"
-            }})}
-            className="input input-bordered w-full"
-            placeholder="Price"
-          />
-          {errors.price && (
-            <p className="text-red-500 text-xs"> {errors.price.message}</p>
-          )}
-        </div>
-        <div>
-          <label className="block text-sm font-medium"> Stock Quantity</label>
-          <input
-            type="number"
-            {...register("stock", { required: true })}
-            className="input input-bordered w-full"
-            placeholder="Stock"
-          />
-          {errors.price && (
-            <p className="text-red-500 text-xs"> This field is required </p>
-          )}
-        </div>
-        {/* DropDown for Categories */}
+          <div>
+            <label className="block text-sm font-medium"> Price</label>
+            <input
+              type="text"
+              {...register("price", {
+                required: "This field is required ",
+                validate: (value) => {
+                  const inputValue = parseFloat(value);
+                  return !isNaN(inputValue) || "Please input a valid number";
+                },
+              })}
+              className="input input-bordered w-full"
+              placeholder="Price"
+            />
+            {errors.price && (
+              <p className="text-red-500 text-xs"> {errors.price.message}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-sm font-medium"> Stock Quantity</label>
+            <input
+              type="number"
+              {...register("stock", { required: true })}
+              className="input input-bordered w-full"
+              placeholder="Stock"
+            />
+            {errors.price && (
+              <p className="text-red-500 text-xs"> This field is required </p>
+            )}
+          </div>
+          {/* DropDown for Categories */}
 
-        <div>
-          <label className="block text-sm font-medium">Category</label>
-          <select
-            {...register("category", { required: true })}
-            className="select select-bordered w-full"
-          >
-            <option value="">Select a category</option>
-            {categories.map((cat) => (
-              <option key={cat.id} value={cat.id}>
-                {cat.name}
-              </option>
-            ))}
-          </select>
-          {errors.category && (
-            <p className="text-red-500 text-xs"> This field is required </p>
-          )}
-        </div>
+          <div>
+            <label className="block text-sm font-medium">Category</label>
+            <select
+              {...register("category", { required: true })}
+              className="select select-bordered w-full"
+            >
+              <option value="">Select a category</option>
+              {categories.map((cat) => (
+                <option key={cat.id} value={cat.id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
+            {errors.category && (
+              <p className="text-red-500 text-xs"> This field is required </p>
+            )}
+          </div>
 
-        <button type="submit" className="btn btn-primary w-full">
-          {" "}
-          Add Product
-        </button>
-      </form>
+          <button type="submit" className="btn btn-primary w-full">
+            {" "}
+            Add Product
+          </button>
+        </form>
+      ) : (
+        <div>
+          <h3 className="text-lg font-medium mb-2"> Upload Product Images</h3>
+          <input
+            type="file"
+            multiple
+            accept="image/*"
+            className="file-input file-input-bordered w-full"
+            onChange={handleImageChange}
+          />
+          {previewImages.length > 0 && (
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 mt-4">
+              {previewImages.map((src, inx) => (
+                <div
+                  key={inx}
+                  className="w-full h-28 sm:h-32 md:h-36 rounded-lg overflow-hidden shadow-md"
+                >
+                  <img
+                    src={src}
+                    alt={`Preview ${inx}`}
+                    className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+
+          <button className="btn btn-primary w-full mt-2">
+            {" "}
+            Upload Images
+          </button>
+        </div>
+      )}
     </div>
   );
 };
